db78a91192acbac94ace57f54dbd5ea71e0485c9
Bernardina Program <bernardi@si-e3.kzn.21-school.ru>
2022-08-20 16:46:21 +0300

    develop

--- src/code.c
+++ src/code.c
@@ -11,26 +11,47 @@ int rocket1_y = 12;
 int rocket2_y = 12;
 int ball_x = 40;
 int ball_y = 13;
-
+int count1 = 0;
+int count2 = 0;
 int main() {
-    while(1) {
+    while (count1 != 21 && count2 != 21) {
         draw();
-        refresh(0, 0;
         getchar();
+        switch (refresh(0, 0)) {
+            case -1:
+                count1++;
+                break;
+            case  1:
+                count2++;
+                break;
+        }
     }
-    
+    draw();
+    printf("The game is finished player number %d WIN", count1 > count2 ? 1 : 2);
     return 0;
 }
 
 void draw() {
     for (int i = 0; i <= height; i++) {
         for (int j = 0; j <= width; j++) {
-            if (i == 0||i == height|| j == 0 || j == width )
-                printf ("#");
+            if (i == 0 || i == height || j == 0 || j == width)
+                printf("#");
+            else if (i == ball_y && j == ball_x)
+                printf("*");
+            else if (j == 40 && i > 0 && i < height)
+                printf("|");
+            else if (i == 3 && j == 37)
+                printf("%d", count1 / 10);
+            else if (i == 3 && j == 38)
+                printf("%d", count1 % 10);
+            else if (i == 3 && j == 42)
+                printf("%d", count2 / 10);
+            else if (i == 3 && j == 43)
+                printf("%d", count2 % 10);
             else if (i >= rocket1_y && rocket1_y + 2 >= i && j == rocket1_x) printf("|");
             else if (i >= rocket2_y && rocket2_y + 2 >= i && j == rocket2_x) printf("|");
-            else if (i == ball_y && j == ball_x) printf("*");
-            else printf(" ");
+            else
+                printf(" ");
         }
         printf("\n");
     }
